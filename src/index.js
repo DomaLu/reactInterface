3b37f0d006c8d7e05e5d6dfb658de6f8bff88ade
Doma Lu <kaku18ccf52@gmail.com>
2016-09-05 15:19:18 +0800

    02_04e Adding lifecycle methods and setting

--- src/index.js
+++ src/index.js
@@ -9,50 +9,34 @@ class MainInterface extends Component {
   constructor(props) {
     super(props)
       this.state = {
-        data: [
-          {
-            "petName": "Buffy",
-            "ownerName": "Hassum Harrod",
-            "aptDate": "2016-06-20 15:30",
-            "aptNotes": "This Chihuahua has not eaten for three days and is lethargic"
-          },
-          {
-            "petName": "Spot",
-            "ownerName": "Constance Smith",
-            "aptDate": "2016-06-24 08:30",
-            "aptNotes": "This German Shepherd is having some back pain"
-          },
-          {
-            "petName": "Goldie",
-            "ownerName": "Barot Bellingham",
-            "aptDate": "2016-06-22 15:50",
-            "aptNotes": "This Goldfish has some weird spots in the belly"
-          },
-          {
-            "petName": "Mitten",
-            "ownerName": "Hillary Goldwyn",
-            "aptDate": "2016-06-21 9:15",
-            "aptNotes": "Cat has excessive hairballs"
-          }
-        ]
+        myAppointments: []
       }
   }
 
+  componentDidMount() {
+    this.serverRequest = $.get('./data.json', (result) => {
+      let tempApts =result
+      this.setState({
+        myAppointments : tempApts
+      })
+    })
+  }
+
   render() {
-    let filteredApts = this.state.data
+    let filteredApts = this.state.myAppointments
 
     filteredApts = filteredApts.map( (item, index) => {
       return (
         <li className='pet-item media' key={index}>
           <div className='pet-info media-body'>
             <div className='pet-head'>
-              <span className='pet-name'>{this.state.data[index].petName}</span>
-              <span className='apt-date pull-right'>{this.state.data[index].aptDate}</span>
+              <span className='pet-name'>{this.state.myAppointments[index].petName}</span>
+              <span className='apt-date pull-right'>{this.state.myAppointments[index].aptDate}</span>
             </div>
             <div className='owner-name'><span className='label-item'>Owner:</span>
-            {this.state.data[index].ownerName}
+            {this.state.myAppointments[index].ownerName}
             </div>
-            <div className='apt-notes'>{this.state.data[index].aptNotes}</div>
+            <div className='apt-notes'>{this.state.myAppointments[index].aptNotes}</div>
           </div>
         </li>
       )
@@ -63,6 +47,11 @@ class MainInterface extends Component {
         <ul className='item-list media-list'>{filteredApts}</ul>
       </div>
   )}
+
+  componentWillUnmount() {
+    this.serverRequest.abort()
+  }
+
 }
 
 
