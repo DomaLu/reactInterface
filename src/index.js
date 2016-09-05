e61d4fd399331451e5b8f5a550508923c1581622
Doma Lu <kaku18ccf52@gmail.com>
2016-09-05 14:12:26 +0800

    02_01e test success

--- src/index.js
+++ src/index.js
@@ -8,14 +8,19 @@ import './css/styles.scss';
 class Init extends Component {
   render() {
     return (
-    <div>
-      <h1>123</h1>
-    </div>
+      <div className='interface'>
+        <h1>Pet Appointments</h1>
+        <ul>
+          <li>Buffy 3:30 PM</li>
+          <li>Spot 8:30 PM</li>
+          <li>Goldie 3:50 PM</li>
+        </ul>
+      </div>
   )}
 }
 
 
 
 ReactDOM.render(
-  <Init />, document.getElementById('root')
+  <Init />, document.getElementById('petAppointments')
 )
